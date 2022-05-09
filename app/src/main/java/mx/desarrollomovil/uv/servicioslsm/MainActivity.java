40ce6b23ffbffeb126d62ce781568c14a8c7a845
Manuel Antonio <zS15013926@estudiantes.uv.mx>
2022-05-08 20:04:30 -0600

    primer commit

---
+++ app/src/main/java/mx/desarrollomovil/uv/servicioslsm/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.desarrollomovil.uv.servicioslsm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
