e76d3e7c324d1854ff803908970dbc90f1553d1e
fredy velázquez mtz <zS15013926@estudiantes.uv.mx>
2022-05-31 04:07:59 -0500

    subida de foto de perfil a firebase

--- app/src/main/java/mx/desarrollomovil/uv/servicioslsm/MainActivity.java
+++ app/src/main/java/mx/desarrollomovil/uv/servicioslsm/MainActivity.java
@@ -1,34 +1,89 @@
 package mx.desarrollomovil.uv.servicioslsm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText etEmailLogin, etPasswordLogin;
     private Button btnEntrar, btnRegistrarse;
 
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseAuth = FirebaseAuth.getInstance();
         etEmailLogin = (EditText) findViewById(R.id.etEmailLogin);
         etPasswordLogin = (EditText) findViewById(R.id.etPasswordLogin);
         btnEntrar = (Button) findViewById(R.id.btnEntrar);
         btnRegistrarse = (Button) findViewById(R.id.btnRegistrarse);
 
+        progressDialog = new ProgressDialog(this);
+
+        btnEntrar.setOnClickListener(view -> {
+            login();
+        });
+
         btnRegistrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                openActivityRegister();
             }
         });
+    }
+
+    private void openActivityRegister() {
+        Intent intent = new Intent(MainActivity.this, Register.class);
+        startActivity(intent);
+    }
+
+    private void login(){
+        String correo = etEmailLogin.getText().toString().trim();
+        String contraseña = etPasswordLogin.getText().toString().trim();
+
+        if(TextUtils.isEmpty(correo)){
+            etEmailLogin.setError("Debes ingresar un correo");
+            etEmailLogin.requestFocus();
+        }else if(TextUtils.isEmpty(contraseña)){
+            Toast.makeText(MainActivity.this, "Falta ingresar la contraseña", Toast.LENGTH_LONG);
+            etPasswordLogin.requestFocus();
+        }else{
+            firebaseAuth.signInWithEmailAndPassword(correo, contraseña).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                       progressDialog.setMessage(getResources().getString(R.string.txt_progressdialog_login));
+                       progressDialog.show();
+                       Toast.makeText(getBaseContext(), getResources().getString(R.string.texto_toast_login), Toast.LENGTH_LONG);
+                       startActivity(new Intent(MainActivity.this, Destacados.class));
+                    }else{
+                        Log.w("TAG", "Error inicio sesion", task.getException());
+                        progressDialog.dismiss();
+                    }
+                }
+            });
+        }
     }
 }
--- app/src/main/java/mx/desarrollomovil/uv/servicioslsm/Register.java
+++ app/src/main/java/mx/desarrollomovil/uv/servicioslsm/Register.java
@@ -1,14 +1,100 @@
 package mx.desarrollomovil.uv.servicioslsm;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class Register extends AppCompatActivity {
+
+    private static final int GALLERY_INTENT = 1;
+
+    private CheckBox cbSordo, cbInterprete;
+    private EditText etNombre, etCorreo, etContraseña, etCiudad;
+    private Button btnInsertarFoto, btnCrearCuenta;
+    private ProgressDialog pd;
+
+    private char tipo;
+    private String userId;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore db;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        cbSordo = (CheckBox) findViewById(R.id.cbSordo);
+        cbInterprete = (CheckBox) findViewById(R.id.cbInterprete);
+        etNombre = (EditText) findViewById(R.id.etNombre);
+        etCorreo = (EditText) findViewById(R.id.etCorreo);
+        etContraseña = (EditText) findViewById(R.id.etContraseña);
+        etCiudad = (EditText)  findViewById(R.id.etCiudad);
+        btnInsertarFoto = (Button)  findViewById(R.id.btnInsertarFoto);
+        btnCrearCuenta = (Button) findViewById(R.id.btnCrearCuenta);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        pd = new ProgressDialog(this);
+        
+        btnInsertarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_INTENT);
+            }
+        });
+        
+        btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_INTENT && requestCode == RESULT_OK){
+            Uri uri = data.getData();
+            StorageReference filePath = storageReference.child("fotos").child(uri.getLastPathSegment());
+            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(Register.this, "tu foto se ha subido correctamente", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void registerUser() {
+        String nombre = etNombre.getText().toString().trim();
+        String correo = etCorreo.getText().toString().trim();
+        String contraseña = etContraseña.getText().toString().trim();
+        String ciudad = etCiudad.getText().toString().trim();
+
+    }
+
 }
