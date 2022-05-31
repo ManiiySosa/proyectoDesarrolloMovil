7ed30b7e8fdc30195412cbd813fd881f797bb45c
Manuel Antonio <raybanss13@gmail.com>
2022-05-30 19:42:11 -0500

    Registro firebase

--- app/src/main/java/mx/desarrollomovil/uv/servicioslsm/MainActivity.java
+++ app/src/main/java/mx/desarrollomovil/uv/servicioslsm/MainActivity.java
@@ -4,13 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etEmailLogin, etPasswordLogin;
+    private Button btnEntrar, btnRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etEmailLogin = (EditText) findViewById(R.id.etEmailLogin);
+        etPasswordLogin = (EditText) findViewById(R.id.etPasswordLogin);
+        btnEntrar = (Button) findViewById(R.id.btnEntrar);
+        btnRegistrarse = (Button) findViewById(R.id.btnRegistrarse);
+
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
